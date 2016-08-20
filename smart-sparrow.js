cdb88463f1aa2349867a3dcdd14d7596399ca9e8
richs1000 <rsimps04@nyit.edu>
2016-08-20 12:08:09 -0400

    Adds SS to JS to Elm interop

--- smart-sparrow.js
+++ smart-sparrow.js
@@ -31,7 +31,7 @@ app.ports.updateMastery.subscribe(function(mastery) {
 
 model.on('change:numerator', function(){
     // var v = model.get('numerator')
-    // alert ("numerator = " + model.numerator + " " + v );
+    alert ("model = " + model);
     app.ports.ssData.send( model );
 });
 
